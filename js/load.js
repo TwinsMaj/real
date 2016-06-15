78bdd0a378cf9a40615a78723a5bf49efef152b3
Maja Taiwo <majataiwo@outlook.com>
2016-06-15 21:33:18 +0100

    dumb security men

---
+++ js/load.js
@@ -0,0 +1,18 @@
+addLoadEvent(prepareSwitches);
+
+function addLoadEvent(func)
+{
+    var oldOnLoad = window.onload;
+
+    if(typeof window.onload !== 'function')
+    {
+        window.onload = func;
+    }
+    else
+    {
+        window.onload = function() {
+            oldOnLoad();
+            func();
+        };
+    }
+}
